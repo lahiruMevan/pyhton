c514695e1346adef479422021693a4d899cfbc88
mevan <lahiru.s@gmail.com>
2022-05-26 22:08:09 +0530

    fin

---
+++ ch10/cell_phone_list.py
@@ -0,0 +1,36 @@
+import cellphone
+
+
+def main():
+    phones = make_list()
+
+    print('Here is the data you entered:')
+    display_list(phones)
+
+def make_list():
+    phone_list = []
+
+    print('Enter data for five phones.')
+    for count in range(1,6):
+        print('Phone number '+str(count)+':')
+        man = input('Enter the manufacturer: ')
+        mod = input('Enter the model number: ')
+        retail = float(input('Enter the retail prices: '))
+        print()
+
+        phone = cellphone.CellPhone(man, mod, retail)
+        phone_list.append(phone)
+
+    
+    return phone_list
+
+def display_list(phone_list):
+    for item in phone_list:
+        print(item.get_manufact())
+        print(item.get_model())
+        print(item.get_retail_price())
+        print()
+
+
+
+main()
