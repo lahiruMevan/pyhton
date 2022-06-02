a26c60ccd0d4105f387546e8317b3b131a05e7b8
mevan <lahiru.s@gmail.com>
2022-06-02 06:07:48 +0530

    class demo

---
+++ ch11-inheritance/vehicles.py
@@ -0,0 +1,42 @@
+class Automobile:
+    def __init__(self, make, model, milage, price):
+        self.__make = make
+        self.__model = model
+        self.__mileage = milage
+        self.__price = price
+
+    def set_make(self, make):
+        self.__make = make
+
+    def set_model(self, model):
+        self.__model = model
+
+    def set_mileage(self, mileage):
+        self.__mileage = mileage
+
+    def set_price(self, price):
+        self.__price = price
+
+    def get_make(self):
+        return self.__make
+
+    def get_model(self):
+        return self.__model
+
+    def get_mileage(self):
+        return self.__mileage
+
+    def get_price(self):
+        return self.__price
+
+
+class Car(Automobile):
+    def __init__(self, make, model, milage, price, doors):
+        Automobile.__init__(self, make, model, milage, price)
+        self.__doors = doors
+
+    def set_doors(self, doors):
+        self.__doors = doors
+
+    def get_doors(self):
+        return self.__doors
